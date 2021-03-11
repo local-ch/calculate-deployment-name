1580a5dd1eea65ffe8140b37cd6bd6e7bd9e51ff
Cedric Wider <wider.cedric@gmail.com>
2021-03-11 11:06:07 +0100

    Set up skeleton
    - Personalize action.yml
    - Delete unnecessary files
    - Skeleton main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const appName = core.getInput('app-name')
+    const branchName = core.getInput('branch-name')
+    let deploymentName;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+    //TODO: Do some magic here!
+    deploymentName = ''
+    
+    core.setOutput('name', deploymentName)
 }
 
 run()
