42fb0f5c3a841bd1b4abf88ae9fc41bbc25fc0be
Cedric Widerl <cedric.wider@gmail.com>
2021-03-11 21:31:41 +0100

    Correct typo
    Somehow half of the function code went missing...

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ async function run(): Promise<void> {
   try {
     const appName = core.getInput('app-name')
     const branchName = core.getInput('branch-name')
-    const deploymentName = calculate(, branchName)
+    const deploymentName = calculate(appName, branchName)
 
     core.debug('Successfully calculated deploymentName')
     core.info(`Setting output 'name' to ${deploymentName}`)
     core.setOutput('name', deploymentName)
+  } catch (error) {
+    core.setFailed(error)
+  }
 }
 
 run()
